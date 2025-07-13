14cf0cd7826731f923640641843a4969c28a9210
amolmhatre262 <143789806+amolmhatre262@users.noreply.github.com>
2025-07-13 18:04:19 +0530

    Added unit test project to solution

---
+++ XUnit_Expencemanagment_Authentication.test/Controllers/AuthenticateControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using ExpenceManagment_AuthenticationSerivices.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnit_Expencemanagment_Authentication.test.Controllers
+{
+    public class AuthenticateControllerTest
+    {
+        [Fact]
+        public void AuthenticateController_SampleUnitTest_Validresult()
+        {
+            //AAA
+
+            //Arrange
+            AuthenticateController controller = new AuthenticateController(null);
+            string ExpectedResult = "This is a sample unit test method.";
+
+            // Act
+            string actualResult = controller.SampleUnitTest();
+
+            // Assert
+            Assert.Equal(ExpectedResult, actualResult);
+        }
+    }
+}
